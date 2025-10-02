ba1ac330cddffe66d235108b4c57af86e859ec09
Alex-SHv <116091695+Alex-SHv@users.noreply.github.com>
2025-10-02 17:04:45 +0300

    Calculator_main

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,19 @@
-print("Easy calculator")
+from init import Init
+from Operation import Calc
+
+num1, num2 = Init()
+while True:
+    opp = input("Enter a operator (+)(-)(*)(/) to exit (Q): ")
+
+    if opp == "q" or opp == "Q":
+        print("Exit to calculator")
+        break
+    result = Calc(num1, num2, opp)
+    if result == "Error, /0":
+        print("Error, /0")
+        break
+    elif result == "Invalid operation":
+        print("Invalid operation")
+        break
+    else:
+        print(result)
